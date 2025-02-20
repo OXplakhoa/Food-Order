9081a40f7527c9ebcf0478db590b4a77c2a014b8
OXplakhoa <OXplakhoa@gmail.com>
2025-02-20 11:55:34 +0700

    Using useFetch to shorten code

--- src/components/AvailableMeals.jsx
+++ src/components/AvailableMeals.jsx
@@ -1,31 +1,16 @@
-import { useEffect, useState } from "react";
 import { useFetch } from "../hooks/useFetch";
 import { fetchAvailableMeals } from "../http";
 import Meals from "./Meals";
-
 export default function AvailableMeals() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
-  const [availableMeals, setAvailableMeals] = useState([]);
-  useEffect(() => {
-    async function fetchAvailableMeals() {
-      try {
-        const res = await fetch("http://localhost:3000/meals");
-        if (!res.ok) return; //Should handle Error later
-        const data = await res.json();
-        setAvailableMeals(data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchAvailableMeals();
-  }, []);
+  const {
+    isLoading,
+    error,
+    data: availableMeals,
+  } = useFetch(fetchAvailableMeals, []);
   return (
     <ul id="meals">
       {availableMeals.map((meal) => (
-        <Meals key={meal.id} meal={meal}/>
+        <Meals key={meal.id} meal={meal} />
       ))}
     </ul>
   );
--- src/http.js
+++ src/http.js
@@ -1,9 +1,8 @@
 export async function fetchAvailableMeals() {
   const res = await fetch("http://localhost:3000/meals");
-  const data = await res.json();
   if (!res.ok) {
     throw new Error("Failed to fetch available places");
   }
+  const data = await res.json();
   return data;
 }
-
