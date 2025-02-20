426ab85d89e28fa6ced248be2afb8dc272af754d
OXplakhoa <OXplakhoa@gmail.com>
2025-02-20 11:47:12 +0700

    Split Meals Component

--- src/components/AvailableMeals.jsx
+++ src/components/AvailableMeals.jsx
@@ -1,19 +1,12 @@
 import { useEffect, useState } from "react";
 import { useFetch } from "../hooks/useFetch";
 import { fetchAvailableMeals } from "../http";
+import Meals from "./Meals";
 
 export default function AvailableMeals() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
   const [availableMeals, setAvailableMeals] = useState([]);
-  //   const {
-  //     isLoading,
-  //     error,
-  //     data: availableMeals,
-  //   } = useFetch(fetchAvailableMeals, []);
-  //   if (error){
-  //     return; //Show Error Comps
-  //   }
   useEffect(() => {
     async function fetchAvailableMeals() {
       try {
@@ -30,27 +23,10 @@ export default function AvailableMeals() {
     fetchAvailableMeals();
   }, []);
   return (
-    // <section id="meals">
-    //   <ul className="meal-item">
-    //     {availableMeals.map((meal) => (
-    //       <li key={meal.id} className="meal-item">
-    //         <article>
-    //           <img src={meal.image} alt={meal.name} />
-    //           <h3>{meal.name}</h3>
-    //           <p className="meal-item-price">{meal.price}</p>
-    //           <p className="meal-item-description">{meal.description}</p>
-    //           <button className="meal-item-actions">Add to Cart</button>
-    //         </article>
-    //       </li>
-    //     ))}
-    //   </ul>
-    // </section>
     <ul id="meals">
-        {availableMeals.map((meal) => (
-            <li key={meal.id}>
-                {meal.name}
-            </li>
-        ))}
+      {availableMeals.map((meal) => (
+        <Meals key={meal.id} meal={meal}/>
+      ))}
     </ul>
   );
 }
