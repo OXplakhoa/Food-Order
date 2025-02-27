0d6950427764a794048188a553f9ddeba6835519
OXplakhoa <OXplakhoa@gmail.com>
2025-02-27 15:45:35 +0700

    Add Loading

--- src/components/AvailableMeals.jsx
+++ src/components/AvailableMeals.jsx
@@ -1,12 +1,20 @@
 import { useFetch } from "../hooks/useFetch";
 import { fetchAvailableMeals } from "../util/http";
 import Meals from "./Meals";
+import Logo from "../assets/logo.jpg";
 export default function AvailableMeals() {
   const {
     isLoading,
     error,
     data: availableMeals,
   } = useFetch(fetchAvailableMeals, []);
+  if (isLoading) {
+    return (
+      <div className="loading-container">
+        <div className="loading-circle"></div>
+      </div>
+    );
+  }
   return (
     <ul id="meals">
       {availableMeals.map((meal) => (
