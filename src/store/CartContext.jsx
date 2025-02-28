481642c1e91eb6ee43a514b3db736978b14dd1bf
OXplakhoa <OXplakhoa@gmail.com>
2025-02-28 21:55:15 +0700

    using switch case for useReducer

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -8,52 +8,42 @@ const CartContext = createContext({
 });
 
 function cartReducer(state, action) {
-  if (action.type === "ADD_ITEM") {
-    const existingCartItemIndex = state.items.findIndex(
-      (item) => item.id === action.item.id
-    );
+  switch (action.type) {
+    case "ADD_ITEM":
+      const existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.item.id
+      );
+      const updatedItems = [...state.items];
+      if (existingCartItemIndex > -1) {
+        const existingItem = state.items[existingCartItemIndex];
+        const updatedItem = {
+          ...existingItem,
+          quantity: existingItem.quantity + 1,
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        updatedItems.push({ ...action.item, quantity: 1 });
+      }
+      return { ...state, items: updatedItems };
+    case "REMOVE_ITEM":
+      const existingCartItem = state.items[existingCartItemIndex];
+      if (existingCartItem.quantity === 1) {
+        updatedItems.splice(existingCartItemIndex, 1);
+      } else {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity - 1,
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+      }
 
-    const updatedItems = [...state.items];
+      return { ...state, items: updatedItems };
+    case "CLEAR_ITEM":
+      return { ...state, items: [] };
 
-    if (existingCartItemIndex > -1) {
-      const existingItem = state.items[existingCartItemIndex];
-      const updatedItem = {
-        ...existingItem,
-        quantity: existingItem.quantity + 1,
-      };
-      updatedItems[existingCartItemIndex] = updatedItem;
-    } else {
-      updatedItems.push({ ...action.item, quantity: 1 });
-    }
-
-    return { ...state, items: updatedItems };
+    default:
+      return state;
   }
-
-  if (action.type === "REMOVE_ITEM") {
-    const existingCartItemIndex = state.items.findIndex(
-      (item) => item.id === action.id
-    );
-    const existingCartItem = state.items[existingCartItemIndex];
-
-    const updatedItems = [...state.items];
-
-    if (existingCartItem.quantity === 1) {
-      updatedItems.splice(existingCartItemIndex, 1);
-    } else {
-      const updatedItem = {
-        ...existingCartItem,
-        quantity: existingCartItem.quantity - 1,
-      };
-      updatedItems[existingCartItemIndex] = updatedItem;
-    }
-
-    return { ...state, items: updatedItems };
-  }
-  if (action.type === "CLEAR_ITEM") {
-    return { ...state, items: [] };
-  }
-
-  return state;
 }
 
 export function CartContextProvider({ children }) {
